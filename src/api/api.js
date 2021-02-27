7f39e098cfadecbd19b0f0e37f6b6283727e05b3
kleutzinger <6435727+kleutzinger@users.noreply.github.com>
2021-02-27 08:35:46 -0800

    start thinking about api/db

---
+++ src/api/api.js
@@ -0,0 +1,14 @@
+const _ = require("lodash")
+          //     betObject = {
+              //     type: "choose1",
+              //     originalMessage: originalMessageObj,
+              //     betOptions ["yes", "no", "pnastayyy"],
+              //     participants [
+              //        {
+              //          betOptionIdx,
+              //          wagerAmount,
+              //          fullMessageObj
+              //        } ...
+              //      ]
+              // }
+
